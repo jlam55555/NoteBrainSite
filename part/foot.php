89c0eff76eb457b77f856a253922327b48262a21
jlam55555 <jlam55555@gmail.com>
2015-01-19 14:09:01 -0500

    Made 'date last modified' dynamic

--- part/foot.php
+++ part/foot.php
@@ -3,7 +3,7 @@
 ?>
 		<div id="footer">
 			<hr />
-			This is the website for NoteBrain, an open-source project by Jonathan Lam. The code is on <a href="https://github.com/jlam55555/NoteBrainSite">GitHub</a> (last update: 1/11/15).<br />
+			This is the website for NoteBrain, an open-source project by Jonathan Lam. The code is on <a href="https://github.com/jlam55555/NoteBrainSite">GitHub</a> (last update: <?php echo date("l, m/d/y",getlastmod()) ?>).<br />
 			&copy; Copyright 2015 Jonathan Lam
 		</div>
 	</body>
